97d3d9058dd75cf532508c65cb5e1ce523ac992c
Marcos Riau <riau222@gmail.com>
2023-08-28 22:58:51 +0200

    Initial Commit

---
+++ DBPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DBPractice
+//
+//  Created by Marcos on 28/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
